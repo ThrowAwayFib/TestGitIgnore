c3a6151da7a0743ff196a0c3b2cbee97fac59590
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-01-27 12:21:24 +0000

    Added a dog class

---
+++ src/com/davegreen/testgitignore/Dog.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.davegreen.testgitignore;
+
+/**
+ *
+ * @author Dave
+ */
+public class Dog
+{
+    private String breed;
+    
+    public Dog(String breed)
+    {
+        this.breed = breed;
+    }
+
+    public String getBreed()
+    {
+        return breed;
+    }
+
+    public void setBreed(String breed)
+    {
+        this.breed = breed;
+    }
+}
